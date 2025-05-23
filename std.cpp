febe4ce9557ad5b83c9ecfc8f6a5ecd8d05f2459
Yanpb <1502709024@qq.com>
2025-05-23 22:35:02 +0800

    杂项

--- std.cpp
+++ std.cpp
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e5 + 10;
-int s[N], top = 0;
+const int N = 1e6 + 10;
+int n, s[N], top;
+char str[N];
 int main() {
-    int n;
-    scanf("%d", &n);
-    int l = 0;
+    scanf("%d%s", &n, str);
     for (int i = 0; i < n; i ++) {
-        int x;
-        scanf("%d", &x);
-        if(s[top] != x) {
-            for (int j = l + 1; j <= x; j ++) {
-                s[++ top] = j;
-                printf("push %d\n", j);
-            }
-            l = x;
-        }
-        puts("pop");
+        if(s[top] !=  str[i]) 
+            s[ ++top] = str[i];
+        else 
+            -- top;
     }
+    for (int i = 1; i <= top; i ++)
+        printf("%c", s[i]);
+    return 0;
 }
