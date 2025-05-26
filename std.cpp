ed4750de34953484a66c02c7ed0a74b409361328
Yanpb <1502709024@qq.com>
2025-05-26 20:58:34 +0800

    链表

--- std.cpp
+++ std.cpp
@@ -1,39 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e5 + 10;
-struct Node 
-{
-    int value;
-    Node* next;
-} l[N], *head;
-int main() 
-{
-    head = NULL;
-    int n; 
-    scanf("%d", &n);
-    for (int i = 0; i < n; i ++) 
-    {
-        scanf("%d", &l[i].value);
-        // 插入位置可能为 链表头部
-        if(head == NULL || l[i].value < head -> value)
-        {
-            l[i].next = head;
-            head = &l[i];
-        } 
-        else 
-        {
-            // 在链表中插入元素，首先要找到最后一个小于 a[i].value 的节点位置
-            Node *p = head;
-            while(p -> next && p -> next -> value < l[i].value)
-                p = p -> next;
-            l[i].next = p -> next;
-            p -> next = &l[i];
-        }
+const int N = 1e5 + 10, M  =1e5 + 10;
+int n, m, ans[N], h[N], e[M], ne[M], k;
+void add(int x, int y) {
+    e[++ k] = y, ne[k] = h[x], h[x] = k;
+}
+
+void dfs(int x, int w) {
+    ans[x] = w;
+    for (int i = h[x]; i; i = ne[i]) {
+        int y = e[i];
+        if(ans[y]) continue;
+        dfs(y, w);
     }
-    Node *p = head;
-    while(p) {
-        printf("%d ", p -> value);
-         p = p-> next;
-    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    scanf("%d%d", &n, &m);
+    // 这里反向新建图
+    for (int i = 0, x, y; i < m;  i++) 
+        scanf("%d%d", &x, &y), add(x, y);
+    for (int i = n; i; i --)
+        if(!ans[i]) dfs(i, i);
+    for (int i = 1; i <= n; i ++)
+        cout << ans[i] << " \n"[i ==n];
     return 0;
 }
