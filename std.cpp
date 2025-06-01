5f689c6467d98c051a9240bb350750f12696cc88
xyb7910 <1502709024@qq.com>
2025-06-01 10:46:16 +0800

    杂项

--- std.cpp
+++ std.cpp
@@ -1,12 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e6 + 10;
-int l[N];
+const int N = 1e5 + 10;
+int n, m, head, tail, l[N], r[N];
+bool st[N];
+inline void init() {
+    head = n + 1, tail = n + 2;
+    r[head] = tail, l[tail] = head;
+}
+
+inline void ins(int u, int v) {
+    int ne = r[u];
+    r[u] = v, l[v] = u;
+    r[v] = ne, l[ne] = v;
+}
+
+inline void del(int v) {
+    int f = l[v], ne = r[v];
+    r[f] = ne, l[ne] = f;
+}
+
 int main() {
-    int n; scanf("%d", &n);
-    for (int i = 1; i <= n; i ++)
-        scanf("%d", &l[i]);
-    int h; scanf("%d", &h);
-    while(h) cout << h << " \n"[!l[h]], h = l[h];
+    scanf("%d", &n);
+    init(), ins(head, 1);
+    for (int i = 2; i <= n; i ++) {
+        int k, p; scanf("%d%d", &k, &p);
+        if(!p) k = l[k];
+        ins(k, i); 
+    }
+    scanf("%d", &m);
+    while(m --) {
+        int x; scanf("%d", &x);
+        if(st[x]) continue;
+        st[x] = 1;
+        del(x);
+    }
+    for (int i = r[head]; i != tail; i = r[i])
+        cout << i << " \n"[r[i] == tail];
     return 0;
 }
